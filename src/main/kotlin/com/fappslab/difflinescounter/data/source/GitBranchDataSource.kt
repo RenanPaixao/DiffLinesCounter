9a4a0254cb26cfe8794376e00ebe87cd75fe65d8
Renan Paixão <renanlemos21@gmail.com>
2026-01-28 23:44:49 -0300

    feat: add branch operations

---
+++ src/main/kotlin/com/fappslab/difflinescounter/data/source/GitBranchDataSource.kt
@@ -0,0 +1,8 @@
+package com.fappslab.difflinescounter.data.source
+
+interface GitBranchDataSource {
+    suspend fun getCurrentBranch(basePath: String?): String?
+    suspend fun listAllBranches(basePath: String?): List<String>
+    suspend fun hasRemoteBranch(basePath: String?, branchName: String): Boolean
+    suspend fun getRemoteName(basePath: String?): String?
+}
