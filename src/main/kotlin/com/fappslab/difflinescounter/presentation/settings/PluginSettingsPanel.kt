1b7db72e27431a7a1cbdf6bcdd749e8a2183b8a2
Renan Paixão <renanlemos21@gmail.com>
2026-01-28 23:49:19 -0300

    feat: create settings ui

---
+++ src/main/kotlin/com/fappslab/difflinescounter/presentation/settings/PluginSettingsPanel.kt
@@ -0,0 +1,26 @@
+package com.fappslab.difflinescounter.presentation.settings
+
+import com.intellij.ui.components.JBLabel
+import com.intellij.ui.components.JBTextField
+import com.intellij.util.ui.FormBuilder
+import javax.swing.JComponent
+import javax.swing.JPanel
+
+class PluginSettingsPanel {
+
+    private val defaultBranchField = JBTextField()
+
+    val panel: JPanel = FormBuilder.createFormBuilder()
+        .addLabeledComponent(JBLabel("Default target branch:"), defaultBranchField, 1, false)
+        .addComponentFillVertically(JPanel(), 0)
+        .panel
+
+    val preferredFocusedComponent: JComponent
+        get() = defaultBranchField
+
+    var defaultBranch: String
+        get() = defaultBranchField.text.trim()
+        set(value) {
+            defaultBranchField.text = value
+        }
+}
